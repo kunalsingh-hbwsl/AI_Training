c34faf339c4188b56bc3aec510c3e3a56e497966
Kunal Singh <kunal.singh@hbwsl.com>
2026-05-21 11:48:38 +0530

    part c and d

--- assignment_1.py
+++ assignment_1.py
@@ -20,21 +20,45 @@ import csv
 #Part B  
 # with open('output/data1.xlsx','r') as excel_output:
 #     reader =csv.reader(excel_output)
-#     next(reader)
 #     i=0
-#     with open('output/is_enabled.xlsx','w') as output2:
+#     first=True
+#     with open('output/data2.xlsx','w') as output2:
 #         writer=csv.writer(output2)
 #         for row in reader:
-#             try:
-#                 is_enabled=row[5].split(',')[6]
-#                 if "Yes" in is_enabled:
-#                     writer.writerow((row[0],row[1],row[2],row[3],row[4],row[5]))
-#             except:
-#                 print("no is_enabled present at",i)
-#             i+=1
+#             if first:
+#                 first=False
+#                 print((row[0],row[1],row[2],row[3],row[4],row[5]))
+#                 writer.writerow((row[0],row[1],row[2],row[3],row[4],row[5]))
+#             else:
+#                 try:
+#                     is_enabled=row[5].split(',')[6]
+#                     if "Yes" in is_enabled:
+#                         writer.writerow((row[0],row[1],row[2],row[3],row[4],row[5]))
+#                 except:
+#                     print("no is_enabled present at",i)
+#                 i+=1
 
-#Part C
-
+#Part C and D
+with open('output/data2.xlsx','r') as part_b:
+    reader=csv.reader(part_b)
+    first=True
+    with open('output/data3.xlsx','w') as part_c:
+        writer=csv.writer(part_c)
+        for row in reader:
+            if first:
+                first=False
+                writer.writerow((row[0],row[1],row[2],row[3],row[4],"specifications_text"))
+                continue
+            else:
+                try:
+                    specifications=row[5].split(',')
+                    for i in specifications:
+                        if "specifications" in i:
+                            writer.writerow((row[0],row[1],row[2],row[3],row[4],i))
+                            break
+                    
+                except:
+                    print("expection occured")
 
 
        
